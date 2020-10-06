944052b73c5a8ae1e75d0d581028251bacfc3059
Zhenyu,Liu <liuzhenyulive@live.com>
2020-10-06 22:21:51 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -56,106 +56,89 @@ export const constantRoutes = [
   },
 
   {
-    path: '/example',
+    path: '/content',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
+    redirect: '/content/article',
+    name: 'Content',
+    meta: { title: 'Content', icon: 'el-icon-s-help' },
     children: [
       {
-        path: 'table',
-        name: 'Table',
+        path: 'article',
+        name: 'Article',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: 'Article', icon: 'table' }
       },
       {
-        path: 'tree',
-        name: 'Tree',
+        path: 'page',
+        name: 'Page',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
+        meta: { title: 'Page', icon: 'tree' }
       }
     ]
   },
-
   {
     path: '/form',
     component: Layout,
     children: [
       {
         path: 'index',
-        name: 'Form',
+        name: 'Comment',
         component: () => import('@/views/form/index'),
         meta: { title: 'Form', icon: 'form' }
       }
     ]
   },
-
   {
-    path: '/nested',
+    path: '/options',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'index',
+        name: 'Options',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Options', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: '/meta',
+    component: Layout,
+    redirect: '/meta/categories',
+    name: 'Meta',
+    meta: { title: 'Meta', icon: 'el-icon-s-help' },
+    children: [
+      {
+        path: 'categories',
+        name: 'Categories',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Categories', icon: 'table' }
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
+        path: 'tags',
+        name: 'Tags',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tags', icon: 'tree' }
+      }
+    ]
+  },
+  {
+    path: 'nacos',
+    component: Layout,
+    children: [
+      {
+        path: 'https://github.com/liuzhenyulive/iblogs.admin',
+        meta: { title: 'Nacos', icon: 'link' }
       }
     ]
   },
 
   {
-    path: 'external-link',
+    path: 'github',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'https://github.com/liuzhenyulive/iblogs.admin',
+        meta: { title: 'Github', icon: 'link' }
       }
     ]
   },
@@ -165,7 +148,7 @@ export const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
