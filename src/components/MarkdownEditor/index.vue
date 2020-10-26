3cdbdcdceaf56729c1267a5a31c0d5279c2c47ab
Zhenyu,Liu <liuzhenyulive@live.com>
2020-10-26 23:16:35 +0800

    impl content edit page

--- src/components/MarkdownEditor/index.vue
+++ src/components/MarkdownEditor/index.vue
@@ -1,27 +1,65 @@
-<!--https://github.com/nhn/tui.editor/tree/master/apps/vue-editor-->
 <template>
-  <editor ref="toastuiEditor" />
+  <editor
+    :initialValue="value"
+    :options="editorOptions"
+    height="500px"
+    previewStyle="vertical"
+  />
 </template>
+
 <script>
 import 'codemirror/lib/codemirror.css'
 import '@toast-ui/editor/dist/toastui-editor.css'
 
 import { Editor } from '@toast-ui/vue-editor'
+import defaultOptions from './default-options'
 
 export default {
+  name: 'MarkdownEditor',
   components: {
     editor: Editor
   },
-  methods: {
-    scroll() {
-      this.$refs.toastuiEditor.invoke('scrollTop', 10)
+  props: {
+    value: {
+      type: String,
+      default: ''
     },
-    moveTop() {
-      this.$refs.toastuiEditor.invoke('moveCursorToStart')
+    options: {
+      type: Object,
+      default() {
+        return defaultOptions
+      }
     },
-    getHtml() {
-      let html = this.$refs.toastuiEditor.invoke('getHtml')
+    mode: {
+      type: String,
+      default: 'markdown'
+    },
+    height: {
+      type: String,
+      required: false,
+      default: '300px'
+    },
+    language: {
+      type: String,
+      required: false,
+      default: 'zh_CN' // https://github.com/nhnent/tui.editor/tree/master/src/js/langs
     }
-  }
+  },
+  data() {
+    return {
+      editorText: 'This is initialValue.'
+    }
+  },
+  computed: {
+    editorOptions() {
+      const options = Object.assign({}, defaultOptions, this.options)
+      options.initialEditType = this.mode
+      options.height = this.height
+      options.language = this.language
+      options.hideModeSwitch = true
+      return options
+    }
+  },
+  methods: {}
 }
 </script>
--- src/views/content/edit.vue
+++ src/views/content/edit.vue
@@ -1,63 +1,13 @@
 <template>
-  <div class="components-container">
-
-    <div class="editor-container">
-      <markdown-editor ref="markdownEditor" v-model="content2" height="500px" />
-    </div>
-    <el-button style="margin-top:80px;" type="primary" icon="el-icon-document" @click="getHtml">
-      Get HTML
-    </el-button>
-    <div v-html="html" />
-  </div>
+  <article-detail :is-edit="true" />
 </template>
 
 <script>
-import MarkdownEditor from '@/components/MarkdownEditor'
+import ArticleDetail from './components/ArticleDetail'
 
-const content = `
-**This is test**
-
-* vue
-* element
-* webpack
-
-`
 export default {
-  name: 'MarkdownDemo',
-  components: { MarkdownEditor },
-  data() {
-    return {
-      content1: content,
-      content2: content,
-      content3: content,
-      content4: content,
-      html: '',
-      languageTypeList: {
-        'en': 'en_US',
-        'zh': 'zh_CN',
-        'es': 'es_ES'
-      }
-    }
-  },
-  computed: {
-    language() {
-      return this.languageTypeList['en']
-    }
-  },
-  methods: {
-    getHtml() {
-      this.html = this.$refs.markdownEditor.getHtml()
-      console.log(this.html)
-    }
-  }
+  name: 'EditForm',
+  components: { ArticleDetail }
 }
 </script>
 
-<style >
-.editor-container{
-  margin-bottom: 30px;
-}
-.tag-title{
-  margin-bottom: 5px;
-}
-</style>
